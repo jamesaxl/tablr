6f29527c044f1d367b958a46a8c2223598451d32
Matt Palmer <matt@silvercreekit.com>
2017-03-10 11:35:10 -0500

    Progress

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+import Schema from './Schema';
 
+
+function Tablr(element) {
+  this.element = resolveElement(element);
+}
+
+
+function resolveElement(element) {
+  if (element instanceof Element) return element;
+  return document.querySelector(element);
+}
+
+
+export default Tablr;
